0529137f1b0881a5c0907407f7fe1fa907a234c4
NtsFranz <ntsluebbers@gmail.com>
2022-08-17 21:13:50 -0400

    tools.echopedia

--- Echopedia/echopedia_fastapi/main.py
+++ Echopedia/echopedia_fastapi/main.py
@@ -9,7 +9,7 @@ from config import *
 
 app = FastAPI()
 
-redirect_uri = "https://echpedia.gg/callback/arena_block_map"
+redirect_uri = "https://tools.echopedia.gg/callback/arena_block_map"
 
 templates = Jinja2Templates(directory="templates")
 
@@ -47,7 +47,6 @@ def get_user(token: str):
     return r.json()
 
 
-@app.get('/more/vrml_match_twitch/{match_id}')
 @app.get('/vrml/match_twitch_link/{match_id}')
 async def vrml_match_twitch(match_id: str):
     try:
@@ -69,8 +68,6 @@ async def login():
 @app.get("/callback/{location}", tags=["Discord Auth"])
 async def callback(response: Response, code: str, location: str):
     print(code)
-    # response.set_cookie(key="session", value="fake-cookie-session-value")
-    # token, refresh_token = await discord.get_access_token(code)
     r = get_access_token(code)
     print(r)
     response = RedirectResponse("/" + location)
@@ -79,20 +76,6 @@ async def callback(response: Response, code: str, location: str):
     return response
 
 
-# @app.get(
-#     "/authenticated",
-#     dependencies=[Depends(discord.requires_authorization)],
-#     response_model=bool,
-#     tags=["Discord Auth"]
-# )
-# async def is_authenticated(token: str = Depends(discord.get_token)):
-#     try:
-#         auth = await discord.isAuthenticated(token)
-#         return auth
-#     except Unauthorized:
-#         return False
-#
-#
 @app.get(
     "/user",
     tags=["Discord Auth"]
@@ -113,25 +96,6 @@ async def get_logged_in(discord_auth_token: Union[str, None] = Cookie(default=No
         return {"logged_in": False}
 
 
-@app.get("/arena_block_map.html", tags=["Pages"], response_class=HTMLResponse)
-async def arena_block_map():
-    return RedirectResponse("/arena_block_map")
-
-
 @app.get("/arena_block_map", tags=["Pages"], response_class=HTMLResponse)
-@app.get("/arena_block_map.html", tags=["Pages"], response_class=HTMLResponse)
 async def arena_block_map(request: Request):
     return templates.TemplateResponse("arena_block_map.html", {"request": request})
-
-# @app.exception_handler(Unauthorized)
-# async def unauthorized_error_handler(_, __):
-#     # return RedirectResponse(discord.oauth_login_url)
-#     return JSONResponse({"error": "Unauthorized"}, status_code=401)
-#
-#
-# @app.exception_handler(RateLimited)
-# async def rate_limit_error_handler(_, e: RateLimited):
-#     return JSONResponse(
-#         {"error": "RateLimited", "retry": e.retry_after, "message": e.message},
-#         status_code=429,
-#     )
